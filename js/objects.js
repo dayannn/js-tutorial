bd331eaa0130b319067b985c4fe787e5a05fbc86
dmuslymov <dmuslymov@moysklad.ru>
2021-11-06 20:46:19 +0300

    functions and objects in JS

---
+++ js/objects.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test');
+    }
+};
+
+delete options.name;
+
+console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`key ${key}.${i}, value = ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`key ${key}, value = ${options[key]}`);
+        counter++;
+    }
+}
+
+console.log(Object.keys(options));
+
+options.makeTest();
+
+// деструктуризация объекта
+const {border, bg} = options.colors;
+console.log(border);
+
--- js/script.js
+++ js/script.js
@@ -1,8 +1,73 @@
 "use strict";
 
-console.log(4 + " - object");
+let numberOfFilms;
 
-let incr = 10,
-    decr = 10;
+function start() {
+     numberOfFilms = +prompt("How many movies have you watched?");
 
-console.log(++incr, --decr);
+     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies have you watched?");
+     }
+}
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Name a recently watched movie', ''),
+              b = prompt('How would you rate it?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Not much, meh');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You\'re an ordinary viewer');
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You're a cinemaholic");
+    } else {
+        console.log('Error');
+    }
+}
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        let genre = prompt(`Your favourite genre #${i+1} ?`);
+        personalMovieDB.genres[i] = genre;
+     }
+}
+
+start();
+
+rememberMyFilms();
+
+detectPersonalLevel();
+
+writeYourGenres();
+
+showMyDB();
